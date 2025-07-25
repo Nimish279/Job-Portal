2dd0c5b69cd414d3d24e6f274f44cfee209a642c
Tushar-D-ops <tushardhakate6@gmail.com>
2025-07-25 19:29:39 +0530

    none dala in res.cookie

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -17,7 +17,7 @@ export const loginUser = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
-            sameSite: "Strict",
+            sameSite: 'None',
             maxAge: 1 * 60 * 60 * 1000, // 1 hour but in cookie form
           });
         res.status(200).json({success:true,message:"User login successful"});
